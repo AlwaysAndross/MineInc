41579abeeea20757002d3ac9b939120be752b5ef
A.Tsatoumas <andrea.tsat@gmail.com>
2024-02-01 13:43:29 -0500

    PlayerMoevment
    Added sprites and code

--- Assets/Scripts/GridGeneration.cs
+++ Assets/Scripts/GridGeneration.cs
@@ -10,48 +10,49 @@ public class GridGeneration : MonoBehaviour
 
     [SerializeField] GameObject dirt, grass, stone, deepslate;
 
-    ArrayList stoneArr = new ArrayList();
-    ArrayList deepArr = new ArrayList();
+    List<int> stoneArr = new List<int>();
+    List<int> deepArr = new List<int>();
 
     void Start()
     {
         Generation();
-        
     }
 
     void Generation()
     {
+        deepGenChance(height, width);
+        stoneGenChance(height, width);
+
         for (int x = 0; x < width; x++)
         {
-            deepArr = deepGenChance(height, width);
-            stoneArr = stoneGenChance(height, width);
             
-            int chanceDeepSpawn = int.Parse(deepArr[0].ToString());
-            int maxDeep = int.Parse(deepArr[1].ToString());
-            int blendDeep = int.Parse(deepArr[2].ToString());
+
+            int chanceDeepSpawn = deepArr[0];
+            int maxDeep = deepArr[1];
+            int blendDeep = deepArr[2];
 
             Console.WriteLine("Deep chance : " + chanceDeepSpawn);
             Console.WriteLine("Deep max : " + maxDeep);
             Console.WriteLine("Deep feather : " + blendDeep);
 
-            int chanceStoneSpawn = int.Parse(stoneArr[0].ToString());
-            int maxStone = int.Parse(stoneArr[1].ToString());
-            int blendStone = int.Parse(stoneArr[2].ToString());
+            int chanceStoneSpawn = stoneArr[0];
+            int maxStone = stoneArr[1]; 
+            int blendStone = stoneArr[0];
 
             Console.WriteLine("Stone chance : " + chanceStoneSpawn);
             Console.WriteLine("Stone max : " + maxStone);
             Console.WriteLine("Stone feather : " + blendStone);
 
-            
+
 
             for (int y = 0; y < height; y++)
             {
                 int rand = UnityEngine.Random.Range(0, 10);
                 if (y < chanceDeepSpawn)
                 {
-                    if ( y > maxDeep & rand > 1)
+                    if (y > maxDeep & rand > 1)
                     {
-                        
+
                         spawnObj(stone, x, y);
                     }
                     else if (y > (maxDeep * 0.9) & rand > 3)
@@ -67,7 +68,7 @@ public class GridGeneration : MonoBehaviour
                 else if (y < chanceStoneSpawn)
                 {
 
-                    if ( y > maxStone & rand > 3)
+                    if (y > maxStone & rand > 3)
                     {
                         spawnObj(dirt, x, y);
                     }
@@ -75,7 +76,7 @@ public class GridGeneration : MonoBehaviour
                     {
                         spawnObj(stone, x, y);
                     }
-                    
+
                 }
                 else
                 {
@@ -87,7 +88,7 @@ public class GridGeneration : MonoBehaviour
             spawnObj(grass, x, height);
 
         }
-        
+
     }
 
     void spawnObj(GameObject obj, int width, int height)
@@ -96,9 +97,8 @@ public class GridGeneration : MonoBehaviour
         obj.transform.parent = this.transform;
     }
 
-    ArrayList stoneGenChance(int height, int width)
+    public void stoneGenChance(int height, int width)
     {
-        ArrayList stoneArr = new ArrayList();
 
         double CalminSpawn = (height / 1.5) + (height * 0.2);
         double CalmaxSpawn = height / 1.5;
@@ -113,12 +113,11 @@ public class GridGeneration : MonoBehaviour
         stoneArr.Add(stoneMaxSpawn);
         int stoneBlendSpawn = (int)CalblendSpawn; //Feathering
         stoneArr.Add(stoneBlendSpawn);
-
-        return stoneArr;
     }
-    ArrayList deepGenChance(int height, int width)
+
+
+    public void deepGenChance(int height, int width)
     {
-        ArrayList deepArr = new ArrayList();
 
         double CalminSpawn = (height / 4) + (height * 0.1);
         double CalmaxSpawn = height / 4;
@@ -126,14 +125,12 @@ public class GridGeneration : MonoBehaviour
 
         int minSpawn = (int)CalminSpawn;
         int maxSpawn = (int)CalmaxSpawn;
-        
+
         int totalDeepSpawn = UnityEngine.Random.Range(maxSpawn, minSpawn); //Randomizing Ints
         deepArr.Add(totalDeepSpawn);
         int deepMaxSpawn = (int)CalmaxSpawn; //Max Deep Value
         deepArr.Add(deepMaxSpawn);
         int deepBlendSpawn = (int)CalblendSpawn; //Feathering
         deepArr.Add(deepBlendSpawn);
-
-        return deepArr;
     }
 }
